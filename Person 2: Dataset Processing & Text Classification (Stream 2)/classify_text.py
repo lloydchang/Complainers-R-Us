c87549ed06f34289f59d36ecd66ddd56563a87de
lloydchang <lloydchang@gmail.com>
2024-08-17 13:30:50 -0700

    feat(classify_text.py): add
    can detect missing file — not found — or wrong file name
    
    ❯ python classify_text.py
    No model was supplied, defaulted to distilbert/distilbert-base-uncased-finetuned-sst-2-english and revision af0f99b (https://huggingface.co/dist
    
    ilbert/distilbert-base-uncased-finetuned-sst-2-english).
    Using a pipeline without specifying a model name and revision in production is not recommended.
    /Users/lloyd/.pyenv/versions/3.12.4/lib/python3.12/site-packages/transformers/tokenization_utils_base.py:1601: FutureWarning: `clean_up_tokenization_spaces` was not set. It will be set to `True` by default. This behavior will be depracted in transformers v4.45, and will be then set to `False` by default. For more details check this issue: https://github.com/huggingface/transformers/issues/31884
      warnings.warn(
    
    I love using GPUs for deep learning!
    [{'label': 'POSITIVE', 'score': 0.9990824460983276}]
    
    I love using Hugging Face models!
    [{'label': 'POSITIVE', 'score': 0.9990824460983276}]
    
    The file cleaned_text.txt.1was not found.

--- Person 2: Dataset Processing & Text Classification (Stream 2)/classify_text.py
+++ Person 2: Dataset Processing & Text Classification (Stream 2)/classify_text.py
@@ -3,35 +3,35 @@ from transformers import pipeline
 # Initialize the classifier
 classifier = pipeline('text-classification', device=0)
 # Use the classifier
-result = classifier("I love using GPUs for deep learning!")
+test_text = "I love using GPUs for deep learning!"
+result = classifier(test_text)
+print ("\n")
+print (test_text)
 print(result)
 
 # Test classification with a simple text
 test_text = "I love using Hugging Face models!"
 results = classifier(test_text)
+print ("\n")
+print (test_text)
+print(result)
+print ("\n")
 
-# Print results
-print(results)
+transcribed_text = "cleaned_text.txt"
 
-# from transformers import pipeline
+try:
+    # Load transcribed text
+    with open(transcribed_text, 'r') as file:
+        transcribed_text = file.read()
 
-# # Initialize the classifier
-# classifier = pipeline('text-classification')
+    # Classify the text
+    results = classifier(transcribed_text)
 
-# try:
-#     # Load transcribed text
-#     with open('transcribed_text.txt', 'r') as file:
-#         transcribed_text = file.read()
+    # Print results
+    print(results)
 
-#     # Classify the text
-#     results = classifier(transcribed_text)
+except FileNotFoundError:
+    print("The file " + transcribed_text + "was not found.")
 
-#     # Print results
-#     print(results)
-
-# except FileNotFoundError:
-#     print("The file 'transcribed_text.txt' was not found.")
-
-# except Exception as e:
-#     print(f"An error occurred: {e}")
-
+except Exception as e:
+    print(f"An error occurred: {e}")
